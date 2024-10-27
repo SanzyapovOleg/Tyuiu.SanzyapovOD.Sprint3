d9e42e413ffc0346a418d0642068180f0717c48d
Tyuiu.SanzyapovOD@outlook.com <oleg.sanzjapov@mail.ru>
2024-10-27 14:25:05 +0500

    Task0

---
+++ Tyuiu.SanzyapovOD.Sprint3.Task0.V29.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.SanzyapovOD.Sprint3.Task0.V29.Lib
+{
+    public class DataService : ISprint3Task0V29
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double sum = 0;
+            int i;
+            for (i = startValue; i <= stopValue; i++)
+            {
+                sum += (Math.Pow(value, 2 * i) + 1 / (i + 1)) * Math.Cos(value);
+            }
+            return Math.Round(sum, 3);
+        }
+    }
+}
